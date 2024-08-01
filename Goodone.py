f36290e9ff174a98b493a7f5f811cecc2408f39e
juniorcarvalho96 <138674681+juniorcarvalho96@users.noreply.github.com>
2024-08-01 16:26:41 -0300

    Create Goodone.py

---
+++ Goodone.py
@@ -0,0 +1,34 @@
+string = input("")
+lista = []
+op = string[6]
+
+
+for i in range(len(string)):
+    if (i <= 12) and (i%4 == 0):
+        lista.append(string[i])
+        
+for i in range(len(lista)):  
+    int(lista[i])
+        
+def operacao(lista):
+    if op == "+":
+        d = lista[0] / lista[1] 
+        k = lista[2] / lista[3]
+        print(d+k)
+        
+    if op == "-":
+        d = lista[0] / lista[1] 
+        k = lista[2] / lista[3]
+        print(d - k)
+        
+    if op == "*":
+        d = lista[0] / lista[1] 
+        k = lista[2] / lista[3]
+        print(d * k)
+        
+    if op == "/":
+        d = lista[0] / lista[1] 
+        k = lista[2] / lista[3]
+        print(d / k)
+        
+
